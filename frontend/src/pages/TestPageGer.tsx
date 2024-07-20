83aa84e03f56f5670953afa494aa95fc13db3742
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-21 01:12:00 +0200

    add logic to show question and to show new question

--- frontend/src/pages/TestPageGer.tsx
+++ frontend/src/pages/TestPageGer.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GermanWordsTest from '../data'
 
-const TestPageGer = () => {
+
+
+const TestPageGer: React.FC = ():React.ReactNode => {
+  const allQuestion = GermanWordsTest
+  const [currentQuestion, setCurrentQuestion] = useState({})
+
+  const nextQuestion = () => {
+    setCurrentQuestion(allQuestion[Math.floor(Math.random() * allQuestion.length)])
+  }
+
+  useEffect(() => {
+    setCurrentQuestion(allQuestion[Math.floor(Math.random() * allQuestion.length)])
+  }, [])
+
+  
   return (
-    <div>Ger</div>
+    <div className="m-16 items-center flex flex-col gap-8">
+      <div>
+        {currentQuestion.correctAnswer}
+      </div>
+      <button onClick={nextQuestion}>Next Question</button>
+    </div>
   )
 }
 
