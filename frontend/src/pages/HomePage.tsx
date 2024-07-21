e777fe29b39cd41d576d973bada15b05e75fe67e
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-21 22:43:46 +0200

    add more tests and comment currently unnecessary code blocks

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,24 +4,24 @@ const HomePage = () => {
   return (
     <div className="m-16 items-center flex flex-col gap-8">
       <h2 className=" font-bold text-2xl">What you would like to do now???</h2>
-      <Link
+      {/* <Link
         to="create_question"
         className=" font-semibold text-2xl border-solid border-2 border-orange-500 p-8 rounded-xl hover:bg-orange-500 hover:text-white  "
       >
         Create Question
-      </Link>
+      </Link> */}
       <Link
         to="test_german"
         className=" font-semibold text-2xl border-solid border-2 border-green-500 p-8 rounded-xl hover:bg-green-500 hover:text-white"
       >
-        German-Rus Test
+        German Test
       </Link>
-      <Link
+      {/* <Link
         to="test_rus"
         className=" font-semibold text-2xl border-solid border-2 border-red-500 p-8 rounded-xl hover:bg-red-500 hover:text-white"
       >
         Rus-German Test
-      </Link>
+      </Link> */}
     </div>
   );
 };
