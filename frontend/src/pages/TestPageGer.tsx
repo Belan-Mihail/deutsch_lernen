456132e16234cd3d759188bb3ac929e9f63c4b25
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-21 15:36:13 +0200

    pre deploy

--- frontend/src/pages/TestPageGer.tsx
+++ frontend/src/pages/TestPageGer.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import GermanWordsTest from "../data";
+import TestType from "../types/TestType";
+
+
+
 
 const TestPageGer: React.FC = (): React.ReactNode => {
   const allQuestion = GermanWordsTest;
-  const [currentQuestion, setCurrentQuestion] = useState({});
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [currentQuestion, setCurrentQuestion] = React.useState<TestType | any>({});
   const [currentAnswer, setCurrentAnswer] = useState("");
-
-  console.log(currentAnswer)
-  
 
   const nextQuestion = () => {
     setCurrentQuestion(
@@ -21,8 +23,6 @@ const TestPageGer: React.FC = (): React.ReactNode => {
     );
   }, []);
 
-  console.log(currentQuestion.correctAnswer)
-  console.log(currentAnswer == currentQuestion.correctAnswer)
 
   return (
     <div className="m-16 items-center flex flex-col gap-8">
@@ -30,61 +30,61 @@ const TestPageGer: React.FC = (): React.ReactNode => {
         <h3 className="font-semibold text-2xl">How to translate this word</h3>
       </div>
       <div>
-        <p className="font-semibold text-2xl my-4">{currentQuestion.word}</p>
+        <p className="font-semibold text-2xl my-4">{currentQuestion?.word}</p>
       </div>
       <div className="grid grid-cols-2 grid-rows-2 gap-16">
         {/* {currentAnswer && currentAnswer == currentQuestion.correctAnswer} */}
         <div
-          onClick={() => setCurrentAnswer(currentQuestion.variantA)}
+          onClick={() => setCurrentAnswer(currentQuestion!.variantA)}
           className={
             
-            currentAnswer === currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantA
+            currentAnswer === currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantA
               ? "font-semibold text-2xl border-solid border-2 bg-green-500 p-8 rounded-xl" :
-              currentAnswer !== currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantA 
+              currentAnswer !== currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantA 
               ? "font-semibold text-2xl border-solid border-2 bg-red-500 p-8 rounded-xl"
               : " font-semibold text-2xl border-solid border-2 border-orange-500 p-8 rounded-xl hover:bg-orange-500 hover:text-white  "
           }
         >
-          {currentQuestion.variantA}
+          {currentQuestion!.variantA}
         </div>
         <div
-          onClick={() => setCurrentAnswer(currentQuestion.variantB)}
+          onClick={() => setCurrentAnswer(currentQuestion!.variantB)}
           className={
             
-            currentAnswer === currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantB
+            currentAnswer === currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantB
               ? "font-semibold text-2xl border-solid border-2 bg-green-500 p-8 rounded-xl" :
-              currentAnswer !== currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantB 
+              currentAnswer !== currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantB 
               ? "font-semibold text-2xl border-solid border-2 bg-red-500 p-8 rounded-xl"
               : " font-semibold text-2xl border-solid border-2 border-orange-500 p-8 rounded-xl hover:bg-orange-500 hover:text-white  "
           }
         >
-          {currentQuestion.variantB}
+          {currentQuestion!.variantB}
         </div>
         <div
-          onClick={() => setCurrentAnswer(currentQuestion.variantC)}
+          onClick={() => setCurrentAnswer(currentQuestion!.variantC)}
           className={
             
-            currentAnswer === currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantC
+            currentAnswer === currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantC
               ? "font-semibold text-2xl border-solid border-2 bg-green-500 p-8 rounded-xl" :
-              currentAnswer !== currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantC 
+              currentAnswer !== currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantC 
               ? "font-semibold text-2xl border-solid border-2 bg-red-500 p-8 rounded-xl"
               : " font-semibold text-2xl border-solid border-2 border-orange-500 p-8 rounded-xl hover:bg-orange-500 hover:text-white  "
           }
         >
-          {currentQuestion.variantC}
+          {currentQuestion!.variantC}
         </div>
         <div
-          onClick={() => setCurrentAnswer(currentQuestion.variantD)}
+          onClick={() => setCurrentAnswer(currentQuestion!.variantD)}
           className={
             
-            currentAnswer === currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantD
+            currentAnswer === currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantD
               ? "font-semibold text-2xl border-solid border-2 bg-green-500 p-8 rounded-xl" :
-              currentAnswer !== currentQuestion.correctAnswer && currentAnswer === currentQuestion.variantD 
+              currentAnswer !== currentQuestion!.correctAnswer && currentAnswer === currentQuestion!.variantD 
               ? "font-semibold text-2xl border-solid border-2 bg-red-500 p-8 rounded-xl"
               : " font-semibold text-2xl border-solid border-2 border-orange-500 p-8 rounded-xl hover:bg-orange-500 hover:text-white  "
           }
         >
-          {currentQuestion.variantD}
+          {currentQuestion!.variantD}
         </div>
       </div>
       <button
