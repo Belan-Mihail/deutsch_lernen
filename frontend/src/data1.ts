79b294ba94f3c1c193f247d06b09f850f9e479c0
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-02-12 10:59:19 +0100

    create data1.ts and add sample data

---
+++ frontend/src/data1.ts
@@ -0,0 +1,16 @@
+
+
+type GernmanSentensesType = {
+    sentence: string
+}
+
+const GermanSentences: GernmanSentensesType[] = [
+    {
+        sentence: 'I will spazieren gehen'
+    },
+    {
+        sentence: 'Ich habe keine Lust, mit dir zu sprechen'
+    }
+]
+
+export default GermanSentences;
